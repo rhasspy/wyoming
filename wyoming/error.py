1fe8ef165c87d5fd4514db348c9451c93d7af91a
Michael Hansen <mike@rhasspy.org>
2023-12-06 13:13:28 -0600

    Add Error event

---
+++ wyoming/error.py
@@ -0,0 +1,31 @@
+"""Error event."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+from .event import Event, Eventable
+
+_ERROR_TYPE = "error"
+
+
+@dataclass
+class Error(Eventable):
+    """Error with text and an optional code."""
+
+    text: str
+    code: Optional[str] = None
+
+    @staticmethod
+    def is_type(event_type: str) -> bool:
+        return event_type == _ERROR_TYPE
+
+    def event(self) -> Event:
+        data: Dict[str, Any] = {"text": self.text}
+        if self.code is not None:
+            data["code"] = self.code
+
+        return Event(type=_ERROR_TYPE, data=data)
+
+    @staticmethod
+    def from_event(event: Event) -> "Error":
+        assert event.data is not None
+        return Error(text=event.data["text"], code=event.data.get("code"))

3a1c341cdcf5371e46fbf99572df9aa327e41e58
Michael Hansen <mike@rhasspy.org>
2024-01-18 14:43:48 -0600

    Fix missing VERSION

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@
 from pathlib import Path
 from setuptools import setup
 
-_DIR = Path(__file__).parent
-_MODULE_DIR = _DIR / "wyoming"
-_VERSION = (_MODULE_DIR / "VERSION").read_text(encoding="utf-8").strip()
+this_dir = Path(__file__).parent
+module_dir = this_dir / "wyoming"
+version_path = module_dir / "VERSION"
+version = version_path.read_text(encoding="utf-8").strip()
+
 
 # -----------------------------------------------------------------------------
 
 setup(
     name="wyoming",
-    version=_VERSION,
+    version=version,
     description="Protocol for Rhasspy Voice Assistant",
     url="http://github.com/rhasspy/wyoming",
     author="Michael Hansen",
     author_email="mike@rhasspy.org",
     license="MIT",
     packages=["wyoming", "wyoming.util"],
+    package_data={"wyoming": [str(p.relative_to(module_dir)) for p in (version_path,)]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
